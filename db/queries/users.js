2f7149dedbba13405fdba43b1af617039c1e1373
JasonSnow123 <m.jason.2018@gmail.com>
2023-02-11 14:30:11 -0500

    adds searching users features

--- db/queries/users.js
+++ db/queries/users.js
@@ -1,10 +1,27 @@
 const db = require('../connection');
 
-const getUsers = () => {
-  return db.query('SELECT * FROM users;')
+const getUsersFromEmail = (email) => {
+  const queryString = `
+  SELECT * FROM users
+  WHERE email = $1;
+  `;
+  return db.query(queryString, [email])
     .then(data => {
-      return data.rows;
+      return data.rows[0];
     });
 };
 
-module.exports = { getUsers };
+const addUsers = (name, email, password) => {
+  const queryString = `
+  INSERT into users
+  VALUES ($1, $2, $3)
+  RETURNING *
+  `;
+  return db.query(queryString, [name, email, password])
+    .then(data => {
+      return data.rows[0];
+    });
+};
+
+module.exports = { getUsersFromEmail, addUsers };
+
