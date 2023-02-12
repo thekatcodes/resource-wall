4ea5805fc2805df460ffe1fa6325cd2b7b1179ff
Katie Liu <katieliu59@gmail.com>
2023-02-12 17:58:29 -0500

    Add router.use to search-router

--- routes/search-router.js
+++ routes/search-router.js
@@ -4,19 +4,25 @@ const bodyParser = require('body-parser');
 const router = express.Router();
 const searchQueries = require('../db/queries/search-queries');
 
-app.use(bodyParser.json());
+// app.use(bodyParser.json());
 
+router.use((req, res, next) => {
+    console.log('inside the SEARCH router!');
+    next();
+  });
 
 //GET 
-router.get('/search', (req, res) => {
+router.get('/', (req, res) => {
     console.log('get request here');
     console.log(req.query);
 
-    searchQueries.getResources(req.query)
-    .then(resources => res.send({resources}))
+    //req.query is passed as a parameter to getResources which retrieves the corresponding resources from the databse
+    searchQueries.getResources(req.query.keyword)
+    //Send resources back to the user as a response to the GET request
+    .then(resources => console.log(resources))
     .catch(e => {
       console.error(e);
-      res.send(e)
+        res.send(e);
     }); 
 
 });
