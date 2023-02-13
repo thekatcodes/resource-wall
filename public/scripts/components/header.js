6a58fc5abeab2f0132814dd60a9abb874832e0bb
Katie Liu <katieliu59@gmail.com>
2023-02-13 18:58:34 -0500

    Refactor updateHeader html to include version if user is not logged in

--- public/scripts/components/header.js
+++ public/scripts/components/header.js
@@ -1,10 +1,15 @@
 $(() => {
+  window.header = {};
+
   const $pageHeader = $("#page-header");
 
-    function updateHeader() {
-        //IF NO USER NAVBAR
-        let header = `
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  function updateHeader(isLoggedIn) {
+    console.log("test header");
+    let header;
+
+    if (!isLoggedIn) {
+      header = `
+        <nav id="header-user-links" class="navbar navbar-expand-lg navbar-light bg-light">
             <a class="navbar-brand" href="#">
                 Logo
             </a>
@@ -23,38 +28,39 @@ $(() => {
             </div>
         </nav>
     `;
-        
-    // IF USER LOGGED IN NAVBAR
-    // let header = `
-    //         <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    //             <a class="navbar-brand" href="#">
-    //                 Logo
-    //             </a>
-    //             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    //                 <span class="navbar-toggler-icon"></span>
-    //             </button>
-    //             <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    //                 <ul class="navbar-nav mr-auto">
-    //                 <li class="nav-item active">
-    //                     <a id="create-resource-btn"class="nav-link">Create resource <span class="sr-only">(current)</span></a>
-    //                 </li>
-    //                 <li class="nav-item">
-    //                     <a class="nav-link" href="#">My resources</a>
-    //                 </li>
-    //                 <form action="/search" method="GET" id="search-resource-form" class="form-inline my-2 my-lg-0">
-    //                     <input class="js-keyword-search form-control mr-sm-2" type="search" placeholder="Search resource" aria-label="Search">
-    //                     <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-    //                 </form>
-    //                 <li class="nav-item">
-    //                     <a class="nav-link active" href="#">Log out</a>
-    //                 </li>
-    //                 <li class="nav-item">
-    //                     <a class="nav-link active" href="#">Profile</a>
-    //                 </li>
-    //                 </ul>
-    //             </div>
-    //         </nav>
-    //     `;
+    } else {
+      // IF USER LOGGED IN NAVBAR
+      header = `
+        <nav id="header-user-links" class="navbar navbar-expand-lg navbar-light bg-light">
+            <a class="navbar-brand" href="#">
+                Logo
+            </a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav mr-auto">
+                <li class="nav-item">
+                    <a id="create-resource-btn" class="nav-link">Create resource</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">My resources</a>
+                </li>
+                <form action="/search" method="GET" id="search-resource-form" class="form-inline my-2 my-lg-0">
+                    <input class="js-keyword-search form-control mr-sm-2" type="search" placeholder="Search resource" aria-label="Search">
+                    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+                </form>
+                <li class="nav-item">
+                    <a class="nav-link active" href="#">Log out</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link active" href="#">Profile</a>
+                </li>
+                </ul>
+            </div>
+        </nav>
+    `;
+    }
 
     $pageHeader.append(header);
   }
@@ -86,7 +92,7 @@ $(() => {
     //   });
   });
   //TO DO: add on click events for navbar options which allows for views_manager.show('component')
-  /* If NO user logged in*/
+  /* If user is NOT logged in*/
   //Render Log in page on click
   //Render register on click
 
