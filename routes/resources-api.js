40e4f52f0f93b29e6ab2ef1c6b9b6fa61e06d169
Katie Liu <katieliu59@gmail.com>
2023-02-14 14:19:31 -0500

    Create router.get for /resources to retrieve searched resources data

--- routes/resources-api.js
+++ routes/resources-api.js
@@ -1,27 +1,36 @@
-const express = require('express');
+const express = require("express");
 
 const router = express.Router();
-const client = require('../db/connection.js');
-const getAllResources = require('../db/queries/getAllResources.js');
-
+const client = require("../db/connection.js");
+const getAllResources = require("../db/queries/getAllResources.js");
 
 router.use((req, res, next) => {
-  console.log('inside the resources router');
+  console.log("inside the resources router");
   next();
 });
 
 // GET /
-router.get('/', (req, res) => {
+router.get("/", (req, res) => {
   getAllResources()
     .then((response) => {
-      console.log('hey')
-      return res.json({response})})
-    .catch(e => {
+      console.log("hey");
+      return res.json({ response });
+    })
+    .catch((e) => {
       console.error(e);
       res.send(e);
-    })
+    });
 });
 
+// GET /resources for search queries
+router.get("/resources", (req, res) => {
+  database
+    .getSearchResources(req.query)
+    .then((resources) => res.send({ resources }))
+    .catch((e) => {
+      console.error(e);
+      res.send(e);
+    });
+});
 
 module.exports = router;
-
