66e84e3ce9ea9fe46bcb297849208b69f06ec175
Giordano Temple <giordanotemple@gmail.com>
2023-02-16 23:45:43 +0000

    main page styling

--- public/scripts/components/resource.js
+++ public/scripts/components/resource.js
@@ -12,6 +12,7 @@ $(() => {
     } else {
       heartIcon = `<i class="favourites fa-regular fa-heart"></i>`
     }
+    console.log(resource)
   return `<div id=${resource.id} class="card resource-card">
        <div>
           <span class="resource-post hide-content">${resource.id}</span>
