6c725ea5bb0a7a14bb9651ac4b51fa45f21007c5
Katie Liu <katieliu59@gmail.com>
2023-02-16 11:00:27 -0500

    Refactor login navbar toggler

--- public/scripts/components/header.js
+++ public/scripts/components/header.js
@@ -13,7 +13,7 @@ $(() => {
                 R
             </a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
+				<i class="fa-solid fa-bars"></i>
             </button>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav mr-auto">
