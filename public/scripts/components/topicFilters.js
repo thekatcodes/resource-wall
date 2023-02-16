c16f239b957702a7f1d319471a5e0c42dd0f2167
Katie Liu <katieliu59@gmail.com>
2023-02-15 19:04:38 -0500

    Create topic filter buttons and click events

---
+++ public/scripts/components/topicFilters.js
@@ -0,0 +1,89 @@
+$(() => {
+  const $topicFilters = $(`
+    <div class="topic-filters">
+    <button id="topic1" class="btn btn-primary" type="submit">School</button>
+    <button id="topic2" class="btn btn-primary" type="submit">Coding</button>
+    <button id="topic3" class="btn btn-primary" type="submit">Games</button>
+    <button id="see-all" class="btn btn-primary" type="submit">See everything</button>
+    </div>
+    `);
+
+  window.$topicFilters = $topicFilters;
+
+  $(document).on("click", "#topic1", function (e) {
+    e.preventDefault();
+    const request = $.ajax({
+      url: "/search",
+      type: "GET",
+      data: { keyword: 'school' },
+      contentType: "application/json; charset=utf-8",
+    });
+      
+    request.done(function (data) {
+        window.newResources.clearResources();
+        for (const item in data) {
+          console.log(data[item]);
+          window.newResources.addResources(data[item]);
+        }
+        views_manager.show("resources");
+      });
+  });
+    
+  $(document).on("click", "#topic2", function (e) {
+    e.preventDefault();
+    const request = $.ajax({
+      url: "/search",
+      type: "GET",
+      data: { keyword: 'coding' },
+      contentType: "application/json; charset=utf-8",
+    });
+      
+    request.done(function (data) {
+        window.newResources.clearResources();
+        for (const item in data) {
+          console.log(data[item]);
+          window.newResources.addResources(data[item]);
+        }
+        views_manager.show("resources");
+      });
+  });
+
+  $(document).on("click", "#topic3", function (e) {
+    e.preventDefault();
+    const request = $.ajax({
+      url: "/search",
+      type: "GET",
+      data: { keyword: 'games' },
+      contentType: "application/json; charset=utf-8",
+    });
+      
+    request.done(function (data) {
+        window.newResources.clearResources();
+        for (const item in data) {
+          console.log(data[item]);
+          window.newResources.addResources(data[item]);
+        }
+        views_manager.show("resources");
+    });
+  });
+    
+  $(document).on("click", "#see-all", function (e) {
+      e.preventDefault();
+      
+    const request = $.ajax({
+      url: "/search",
+      type: "GET",
+      contentType: "application/json; charset=utf-8",
+    });
+      
+    request.done(function (data) {
+        window.newResources.clearResources();
+        for (const item in data) {
+          console.log(data[item]);
+          window.newResources.addResources(data[item]);
+        }
+        views_manager.show("resources");
+    });
+  });
+});
+
