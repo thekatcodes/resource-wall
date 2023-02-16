4e5ab4cdb0444267d14092c5c4442da9ae377a07
JasonSnow123 <m.jason.2018@gmail.com>
2023-02-16 12:59:26 -0500

    Adds form styling to login, register and submission

--- public/scripts/components/loginForm.js
+++ public/scripts/components/loginForm.js
@@ -1,14 +1,25 @@
 $(() => {
   const $loginForm = $(`
     <form action="/api/login" method="POST" class="login">
-      <div class="error-message"></div>
-      <h3>Log in</h3>
-      <h3>Please fill in fields</h3>
-      <label for="email">Email</label>
-      <input type="email" name="email" class="email-form">
-      <label for="password">Password</label>
-      <input type="password" name="password" class="password-form">
-      <button type="submit" class="btn btn-info" id="login" disabled="disabled">Submit</button>
+      <div class="form">
+      <div class="d-flex align-items-center justify-content-center">
+        <h1 class="form-title">Log in</h1>
+      </div>
+      <div class="d-flex align-items-center justify-content-center">
+        <div class="error-message"></div>
+      </div>
+      <div class="form-group">
+        <label for="email">Email</label>
+        <input type="email" name="email" class="form-control email-form input-form">
+      </div>
+      <div class="form-group">
+        <label for="password">Password</label>
+        <input type="password" name="password" class="form-control password-form input-form">
+      <div class="form-group">
+      <div class="d-flex align-items-center justify-content-center">
+      <button type="submit" class="btn btn-info" id="login" disabled="disabled">Login</button>
+      </div>
+      </div>
     </form>
     `);
   window.$loginForm = $loginForm;
@@ -18,12 +29,13 @@ $(() => {
     event.preventDefault();
     $.post("/api/login", {info : $(this).serialize()})
         .done((response) => {
-            $("#page-header").empty();
-            header.update(response);
-            views_manager.show('resources');
         if (!response) {
           $(".error-message").empty();
-          $(".error-message").append("<h1>Wrong Password</h1>");
+          $(".error-message").append('<div class="alert alert-danger"><h3>Incorrect information credentials</h3></div>');
+        } else {
+          $("#page-header").empty();
+          header.update(response);
+          views_manager.show('resources');
         }
       });
   });
--- public/scripts/components/submissionForm.js
+++ public/scripts/components/submissionForm.js
@@ -1,20 +1,38 @@
 $(() => {
   const $submissionForm = $(`
   <form action="/api/resource" method="POST" class="resource-submission">
-    <div class="error-message"></div>
-    <h2>Submit a new resource</h2>
-    <label for="title">Title</label>
-    <input type="text" name="title" class="title-form">
-    <label for="imageURL">Image URL</label>
-    <input type="text" name="imageURL" class="image-url-form">
-    <label for="externalURL">Resource URL</label>
-    <input type="text" name="externalURL" class="resource-url-form">
-    <label for="description">Description</label>
-    <textarea name="description" rows="5" cols="80" class="description-form"></textarea>
-    <label for="tags">Tags</label>
-    <textarea name="tags" rows="5" cols="80" class="tag-form"></textarea>
-    <button type="submit" class="btn btn-info">Submit</button>
-    </form>
+    <div class="form">
+      <div class="d-flex align-items-center justify-content-center form-title">
+        <h1>Submit a new resource</h1>
+      </div>
+      <div class="d-flex align-items-center justify-content-center">
+        <div class="error-message"></div>     
+      </div>
+      <div class="form-group">
+        <label for="title">Title</label>
+        <input type="text" name="title" class="form-control title-form" required>
+      </div>
+      <div class="form-group">
+        <label for="imageURL">Image URL</label>
+        <input type="text" name="imageURL" class="form-control image-url-form">
+      </div>
+      <div class="form-group">
+        <label for="externalURL">Resource URL</label>
+        <input type="text" name="externalURL" class="form-control resource-url-form" required>
+      </div>
+      <div class="form-group">
+        <label for="description">Description</label>
+        <textarea name="description" rows="5" cols="80" class="form-text form-control description-form"></textarea>
+      </div>
+      <div class="form-group">
+        <label for="tags">Tags</label>
+        <textarea name="tags" rows="5" cols="80" class="form-text form-control tag-form" required></textarea>
+      </div>
+      <div class="d-flex align-items-center justify-content-center form-title">
+        <button type="submit" class="btn btn-info">Submit</button>
+      </div>
+    </div>
+  </form>
   `);
 
   window.$submissionForm = $submissionForm;
@@ -24,7 +42,7 @@ $(() => {
     $.post("/api/resources/submission", {info : $(this).serialize()})
       .then((res) => {
         if (res === "") {
-          $(".error-message").append("<h1>Details need to be filled out</h1>");
+          $(".error-message").append('<div class="alert alert-danger"><h3>Details need to be filled out</h3></div>');
         } else {
           console.log("show main page");
         }
